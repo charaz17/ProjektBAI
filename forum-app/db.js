ff1ffbf69f664fc0bb4816cedc50d28f86a27357
charaz17 <hubertch1999@gmail.com>
2024-06-14 17:41:13 +0200

    SQLInjection secure + vulnerable version, csrf attack

--- forum-app/db.js
+++ forum-app/db.js
@@ -1,5 +1,3 @@
-// db.js
-
 const { Sequelize, DataTypes } = require('sequelize');
 
 // Create a new Sequelize instance
--- forum-app/server.js
+++ forum-app/server.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const moment = require('moment');
-const sequelize = require('./db'); // Import the sequelize instance
+const session = require('express-session');
+const { sequelize, User, Post } = require('./db'); // Import the sequelize instance and models
+const { QueryTypes } = require('sequelize');
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
-
+app.use(session({
+  secret: 'your_secret_key',
+  resave: false,
+  saveUninitialized: true
+}));
 app.set('view engine', 'ejs');
+
+// Middleware to set userId in locals for all routes
+app.use((req, res, next) => {
+  res.locals.userId = req.session.user ? req.session.user.id : null;
+  next();
+});
 
 // Routes
 
@@ -23,10 +34,8 @@ app.get('/register', (req, res) => {
 
 app.post('/register', async (req, res) => {
   const { username, password } = req.body;
-  const query = `INSERT INTO "Users" ("username", "password", "createdAt", "updatedAt") VALUES ('${username}', '${password}', NOW(), NOW())`;
-  
   try {
-    await sequelize.query(query);
+    const user = await User.create({ username, password });
     res.render('message', { message: 'Account created successfully, you can login now.' });
   } catch (err) {
     console.error('Error inserting user:', err.message);
@@ -38,6 +47,7 @@ app.get('/login', (req, res) => {
   res.sendFile(__dirname + '/views/login.html');
 });
 
+//VULNERABLE
 app.post('/login', async (req, res) => {
   const { username, password } = req.body;
   const query = `SELECT "id", "username", "password", "createdAt", "updatedAt" FROM "Users" WHERE "username" = '${username}' AND "password" = '${password}'`;
@@ -56,16 +66,39 @@ app.post('/login', async (req, res) => {
   }
 });
 
+//SECURE
+// app.post('/login', async (req, res) => {
+//   const { username, password } = req.body;
+//   sanitized_username = username;
+//   sanitized_password = password;
+//   sanitized_username = sanitized_username.replace(/'/g, "").replace(/-/g, "");
+//   sanitized_password = sanitized_password.replace(/'/g, "").replace(/-/g, "");
+//   const query = `SELECT "id", "username", "password", "createdAt", "updatedAt" FROM "Users" WHERE "username" = '${sanitized_username}' AND "password" = '${sanitized_password}'`;
+
+//   try {
+//     const [results, metadata] = await sequelize.query(query);
+//     const user = results[0];
+//     if (user) {
+//       res.redirect(`/forum?user_id=${user.id}`);
+//     } else {
+//       res.redirect('/login');
+//     }
+//   } catch (err) {
+//     console.error('Error logging in:', err.message);
+//     res.redirect('/login');
+//   }
+// });
+
+
+
 app.get('/forum', async (req, res) => {
   const user_id = req.query.user_id;
-  const query = `
-    SELECT "Posts"."id", "Posts"."user_id", "Posts"."content", "Posts"."timestamp", "Users"."username"
-    FROM "Posts"
-    INNER JOIN "Users" ON "Posts"."user_id" = "Users"."id"
-  `;
-
   try {
-    const [posts, metadata] = await sequelize.query(query);
+    const posts = await Post.findAll({
+      include: [{ model: User, attributes: ['username'] }],
+      order: [['createdAt', 'DESC']]
+    });
+
     res.render('forum', { posts, user_id });
   } catch (err) {
     console.error('Error fetching posts:', err.message);
@@ -80,16 +113,27 @@ app.get('/post', (req, res) => {
 
 app.post('/post', async (req, res) => {
   const { user_id, content } = req.body;
-  const query = `INSERT INTO "Posts" ("user_id", "content", "timestamp") VALUES (${user_id}, '${content}', NOW())`;
-
   try {
-    await sequelize.query(query);
+    await Post.create({ user_id, content });
     res.redirect(`/forum?user_id=${user_id}`);
   } catch (err) {
     console.error('Error creating post:', err.message);
     res.redirect(`/forum?user_id=${user_id}`);
   }
 });
+
+// CSRF Attack route
+app.get('/csrf_attack', (req, res) => {
+  try {
+    const userId = req.session.user ? req.session.user.id : null;
+    res.render('csrf_attack', { userId });
+  } catch (err) {
+    console.error('Error rendering CSRF attack page:', err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+
 
 // Start server
 app.listen(port, () => {
